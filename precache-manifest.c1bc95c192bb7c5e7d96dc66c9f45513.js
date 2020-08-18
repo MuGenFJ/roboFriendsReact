56fb31850a8b00465323180d0d924699526fb84e
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2020-08-18 20:18:37 +0200

    Updates

---
+++ precache-manifest.c1bc95c192bb7c5e7d96dc66c9f45513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66fe9691160bb349a3ce351c83bd5c82",
+    "url": "/roboFriendsReact/index.html"
+  },
+  {
+    "revision": "eca4883b9ef83e7678dd",
+    "url": "/roboFriendsReact/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "73446db38a978998ca1b",
+    "url": "/roboFriendsReact/static/css/main.16ee5cd3.chunk.css"
+  },
+  {
+    "revision": "eca4883b9ef83e7678dd",
+    "url": "/roboFriendsReact/static/js/2.aec8e782.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboFriendsReact/static/js/2.aec8e782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73446db38a978998ca1b",
+    "url": "/roboFriendsReact/static/js/main.72f1e94e.chunk.js"
+  },
+  {
+    "revision": "30bb41ae5ce8ece12030",
+    "url": "/roboFriendsReact/static/js/runtime-main.e15d59e0.js"
+  }
+]);
